3b7b7babcaed1d5a9f9f41a52c3ed7e65b7f5772
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-10 16:29:48 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -31,5 +31,5 @@ def remove_strawberry(contacts)
   contacts.each do |name, data|
       data.each do |attribute, details|
       if attribute == "favorite_ice_cream_flavors"
-        
+        details.delete if 
 end
